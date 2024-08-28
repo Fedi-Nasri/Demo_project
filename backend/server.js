8325e1e3944d5e0d906a13b6068ff77d48f9fdac
tesnim01 <66748341+tesnim01@users.noreply.github.com>
2024-08-28 23:53:24 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('Connected to MongoDB');
     app.listen(process.env.PORT || 5000, () => {
