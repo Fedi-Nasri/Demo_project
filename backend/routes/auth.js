e8832adb93f69bcf54c562d12bf91ba325c206bd
tesnim01 <66748341+tesnim01@users.noreply.github.com>
2024-08-28 23:52:38 +0100

    Update auth.js crud

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const { register, login } = require('../controllers/authController');
+const { register, login, getUser, updateUser, deleteUser, getAllUsers } = require('../controllers/authController');
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
+router.get('/users/:id', getUser);
+router.put('/users/:id', updateUser);
+router.delete('/users/:id',deleteUser);
+router.get('/users', getAllUsers);
 
 module.exports = router;
